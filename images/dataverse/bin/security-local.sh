0afd737507d4d6e1f98b9715216af471b55d59d3
Kris Dekeyser <kris@dva.be>
2021-06-18 12:20:46 +0200

    safer security switching

--- images/dataverse/bin/security-local.sh
+++ images/dataverse/bin/security-local.sh
@@ -6,6 +6,5 @@ echo "Restricting admin API to localhost only"
 api PUT 'admin/settings/:BlockedApiPolicy' -d 'localhost-only'
 api PUT 'admin/settings/:BlockedApiEndpoints' -d 'admin,builtin-users'
 api DELETE 'admin/settings/:BlockedApiKey'
-echo
-
 export API_LOCAL="true"
+echo
--- images/dataverse/bin/security-token.sh
+++ images/dataverse/bin/security-token.sh
@@ -3,9 +3,8 @@
 . "${SCRIPT_DIR}/config"
 
 echo "Protecting admin API with token"
+export API_LOCAL="false"
 api PUT 'admin/settings/:BlockedApiKey' -d "${API_KEY}"
 api PUT 'admin/settings/:BlockedApiPolicy' -d 'unblock-key'
 api PUT 'admin/settings/:BlockedApiEndpoints' -d 'admin,builtin-users'
 echo
-
-export API_LOCAL="false"
