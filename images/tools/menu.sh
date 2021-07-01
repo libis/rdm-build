1a9798a27f280113fcebc87cd1378e3798d7a650
Kris Dekeyser <kris.dekeyser@libis.be>
2021-07-01 19:13:45 +0200

    Tools: add storage report

--- images/tools/menu.sh
+++ images/tools/menu.sh
@@ -7,6 +7,7 @@ usage() {
   echo 'Menu'
   echo '===='
   echo '1. Export Datasets'
+  echo '2. Storage report'
   echo ''
   echo '0. Exit'
 }
@@ -20,11 +21,14 @@ while [ "$x" != "0" ]; do
       ;;
     "1")
       ruby ${HOME}/export_datasets.rb
-      read -n 1 -s -r -p "Press any key to continue"
+      ;;
+    "2")
+      ruby ${HOME}/storage_report.rb
       ;;
     *)
       ;;
   esac
+  read -n 1 -s -r -p "Press any key to continue"
   usage
   read -n 1 -p 'Choice: ' x
   echo
