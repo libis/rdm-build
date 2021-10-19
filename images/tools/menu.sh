ae49e32f170287e76be1e61889367332131308ac
Kris Dekeyser <kris.dekeyser@libis.be>
2021-10-19 16:09:37 +0200

    Add new tools to menu

--- images/tools/menu.sh
+++ images/tools/menu.sh
@@ -9,6 +9,9 @@ usage() {
   echo '1. Export Datasets'
   echo '2. Storage report'
   echo ''
+  echo 'a. Capture metadata properties'
+  echo 'b. Apply custom translations'
+  echo ''
   echo '0. Exit'
 }
 
@@ -24,6 +27,12 @@ while [ "$x" != "0" ]; do
       ;;
     "2")
       ruby ${HOME}/storage_report.rb
+      ;;
+    "a")
+      ruby ${HOME}/metadata_properties.rb /opt/dv_config/overwrite/metadatablocks /opt/languages
+      ;;
+    "b")
+      ruby ${HOME}/customize_properties.rb /opt/dv_config/data/properties /opt/languages
       ;;
     *)
       ;;
