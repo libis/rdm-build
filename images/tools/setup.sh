f149ec9aa652f2fec30e7abac69f20a732208b17
Kris Dekeyser <kris@dva.be>
2021-12-04 04:37:25 +0100

    Tools: URL configuration

--- images/tools/setup.sh
+++ images/tools/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export PATH=${HOME}:${HOME}/scripts:${PATH}
 [[ -f /run/secrets/api/adminkey ]] && API_TOKEN="$(cat /run/secrets/api/adminkey)"
-API_URL="http://${DATAVERSE_SERVICE_HOST}:${DATAVERSE_SERVICE_PORT_HTTP}/api/v1"
+API_URL="${DATAVERSE_URL}/api/v1"
 echo "API environment initialized:"
 echo "  API_URL: ${API_URL}"
 echo "  API_TOKEN: ${API_TOKEN}"
