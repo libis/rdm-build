6fa45ac29b68193e3f39d31f7d8726346b625da3
Kris Dekeyser <kris.dekeyser@libis.be>
2021-12-06 16:15:08 +0100

    Tools: cleanup tool scripts

--- images/tools/export_datasets.rb
+++ images/tools/export_datasets.rb
@@ -3,15 +3,34 @@
 require 'dotenv/load'
 require 'dataverse'
 require 'fileutils'
+require 'date'
 
-root_dv = Dataverse::Dataverse.id('rdr')
+ENV['DATA_DIR'] ||= '/opt/data'
+ENV['COLLECTION'] ||= 'rdr'
 
-root_dv.each_dataset do |ds|
+ENV['FROM_DATE_FILE'] ||= File.join(ENV['DATA_DIR'], 'exports', 'last_export_date')
+
+from_date = File.exist?(ENV['FROM_DATE_FILE']) ? Date.parse(File.read(ENV['FROM_DATE_FILE'])) : Date.new(2021)
+
+puts "Extracting Datasets from Dataverse Collection '#{ENV['COLLECTION']}'"
+puts "published between #{from_date} and #{Date.today} ..."
+
+dv = Dataverse::Dataverse.id(ENV['COLLECTION'])
+i = 0
+dv.each_dataset do |ds|
   next unless ds.version(:published)
+  next unless Date.parse(ds.publicationDate) >= from_date
+  next unless Date.parse(ds.publicationDate) < Date.today
   data = ds.export_metadata('rdm')
-  filename = "exports/#{ds['identifier']}.json"
-  filepath = "#{ENV['DATA_DIR']}/#{filename}"
+  filename = File.join('exports', "#{ds['identifier']}.json")
+  filepath = File.join(ENV['DATA_DIR'], filename)
   FileUtils.mkdir_p(File.dirname(filepath))
-  puts "#{filename}"
+  puts "#{filename}\t#{ds['persistentUrl']}\t#{ds.metadata['title']}"
   File.open(filepath, 'wt') { |f| f.write JSON.pretty_generate(data) }
+  i += 1
 end
+
+File.write(ENV['FROM_DATE_FILE'], Date.today.to_s)
+
+puts "-------------"
+puts "#{i} datasets exported."
--- images/tools/storage_report.rb
+++ images/tools/storage_report.rb
@@ -5,12 +5,16 @@ require 'dataverse'
 require 'fileutils'
 require 'csv'
 
+ENV['DATA_DIR'] ||= '/opt/data'
+ENV['COLLECTION'] ||= 'rdr'
+ENV['ORCID_FILE'] ||= 'orcid.csv'
+
 orcid_map = {}
-CSV.foreach("#{ENV['DATA_DIR']}/orcid.csv") do |row|
+CSV.foreach(File.join({ENV['DATA_DIR'], ENV['ORCID_FILE']) do |row|
   orcid_map[row.last.downcase] = row.first.downcase
 end
 
-root_dv = Dataverse::Dataverse.id('rdr')
+root_dv = Dataverse::Dataverse.id(ENV['COLLECTION'])
 
 storage_map = {}
 error_ds = []
@@ -29,10 +33,13 @@ root_dv.each_dataset do |ds|
   storage_map[unr] += ds.size
 end
 
+ENV['REPORTS_DIR'] ||= File.join(ENV['DATA_DIR'], 'reports')
+FileUtils.mkdir_p(ENV['REPORTS_DIR'])
+
 now = DateTime.now
-filepath = "#{ENV['DATA_DIR']}/reports/storage-#{now.strftime('%Y-%m-%d')}.csv"
-FileUtils.mkdir_p(File.dirname(filepath))
+
+filepath = File.join(ENV['REPORTS_DIR'], "storage-#{now.strftime('%Y-%m-%d')}.csv")
 File.open(filepath, 'wt') { |f| storage_map.each { |unr,size| f.puts "#{unr},#{size}" } }
 
-filepath = "#{ENV['DATA_DIR']}/reports/storage-#{now.strftime('%Y-%m-%d')}.errors"
+filepath = File.join(ENV['REPORTS_DIR'], "storage-#{now.strftime('%Y-%m-%d')}.errors")
 File.open(filepath, 'wt') { |f| error_ds.each { |id| f.puts "#{id}" } }
