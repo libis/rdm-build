39e2d4229af5fa3a981881b4f242e596ab230d01
Kris Dekeyser <kris@dva.be>
2021-12-08 21:47:32 +0100

    Tools image optimizations

--- images/tools/export_datasets.rb
+++ images/tools/export_datasets.rb
@@ -3,7 +3,6 @@
 require 'dotenv/load'
 require 'dataverse'
 require 'fileutils'
-require 'date'
 
 ENV['DATA_DIR'] ||= File.join('/', 'opt', 'data')
 ENV['EXPORT_DIR'] ||= File.join(ENV['DATA_DIR'], 'exports')
@@ -13,27 +12,28 @@ ENV['COLLECTION'] ||= ':root'
 
 ENV['FROM_DATE_FILE'] ||= File.join(ENV['EXPORT_DIR'], 'last_export_date')
 
-from_date = File.exist?(ENV['FROM_DATE_FILE']) ? Date.parse(File.read(ENV['FROM_DATE_FILE'])) : Date.new(2021)
+from_date = File.exist?(ENV['FROM_DATE_FILE']) ? Time.parse(File.read(ENV['FROM_DATE_FILE'])) : Time.new(2021)
+to_date = Time.now
 
 dv = Dataverse::Dataverse.id(ENV['COLLECTION'])
 
 puts "Extracting Datasets from Dataverse Collection '#{dv['name']}'"
-puts "published between #{from_date} and #{Date.today} ..."
+puts "published between #{from_date} and #{to_date} ..."
 
 i = 0
 dv.each_dataset do |ds|
   next unless ds.version(:published)
-  next unless Date.parse(ds.publicationDate) >= from_date
-  next unless Date.parse(ds.publicationDate) < Date.today
+  next unless ds.published >= from_date
+  next unless ds.published < to_date
   data = ds.export_metadata('rdm')
   filename = "#{ds['identifier']}.json"
   filepath = File.join(ENV['EXPORT_DIR'], filename)
-  puts "#{filename}\t#{ds['persistentUrl']}\t#{ds.metadata['title']}"
+  puts "#{filename}\t#{ds['persistentUrl']}\t#{ds.title(version: :published)}"
   File.open(filepath, 'wt') { |f| f.write JSON.pretty_generate(data) }
   i += 1
 end
 
-File.write(ENV['FROM_DATE_FILE'], Date.today.to_s)
+File.write(ENV['FROM_DATE_FILE'], to_date.to_s)
 
 puts "-------------"
 puts "#{i} datasets exported."
--- images/tools/storage_report.rb
+++ images/tools/storage_report.rb
@@ -5,16 +5,19 @@ require 'dataverse'
 require 'fileutils'
 require 'csv'
 
-ENV['DATA_DIR'] ||= '/opt/data'
-ENV['COLLECTION'] ||= ':root'
-ENV['ORCID_FILE'] ||= File.join({ENV['DATA_DIR'], 'orcid.csv')
+ENV['DATA_DIR']    ||= '/opt/data'
+ENV['COLLECTION']  ||= ':root'
+ENV['ORCID_FILE']  ||= File.join(ENV['DATA_DIR'], 'orcid.csv')
 ENV['REPORTS_DIR'] ||= File.join(ENV['DATA_DIR'], 'reports')
 
 FileUtils.mkdir_p(ENV['REPORTS_DIR'])
 
 unless File.exist?(ENV['ORCID_FILE'])
   puts "ERROR: ORCID file '#{ENV['ORCID_FILE']}'does not exist"
+  exit
 end
+
+puts "Using ORCID file '#{ENV['ORCID_FILE']}."
 
 orcid_map = {}
 CSV.foreach(ENV['ORCID_FILE']) do |row|
@@ -24,10 +27,9 @@ end
 dv = Dataverse::Dataverse.id(ENV['COLLECTION'])
 
 storage_map = {}
-error_ds = []
+error_ds = {}
 
 puts "Collecting Datasets storage from Dataverse Collection '#{dv['name']}' ..."
-puts "Using ORCID file '#{ENV['ORCID_FILE']}."
 
 dv.each_dataset do |ds|
   authors = ds.metadata['author']
@@ -37,7 +39,7 @@ dv.each_dataset do |ds|
   end
   unless unr
     puts "WARNING: Dataset #{ds.pid} does not have an author to contribute the storage to (not a known ORCID)."
-    error_ds << ds.pid
+    error_ds[ds.pid] = ds.size
     next
   end
   storage_map[unr] ||= 0
@@ -45,13 +47,20 @@ dv.each_dataset do |ds|
 end
 
 now = DateTime.now
+csv_options = {write_headers: true, quote_char: '"', row_sep: "\n", col_sep: ','}
 
 filepath = File.join(ENV['REPORTS_DIR'], "storage_#{now.strftime('%Y-%m-%d')}.csv")
-File.open(filepath, 'wt') { |f| f.puts '"u-number","storage in bytes"'; storage_map.each { |unr,size| f.puts "#{unr},#{size}" } }
+CSV.open(filepath, 'w', headers: ['u-number', 'storage in bytes'], **csv_options) do |csv|
+  storage_map.each { |h| csv << h }
+end
+# File.open(filepath, 'wt') { |f| f.puts '"u-number","storage in bytes"'; storage_map.each { |unr,size| f.puts "#{unr},#{size}" } }
 
 puts "Storage report '#{File.basename(filepath)}' written. #{storage_map.size} entries."
 
-filepath = File.join(ENV['REPORTS_DIR'], "storage_#{now.strftime('%Y-%m-%d')}.errors")
-File.open(filepath, 'wt') { |f| error_ds.each { |id| f.puts "#{id}" } }
+filepath = File.join(ENV['REPORTS_DIR'], "storage-errors_#{now.strftime('%Y-%m-%d')}.csv")
+CSV.open(filepath, 'w', headers: ['DOI', 'storage in bytes'], **csv_options) do |csv|
+  error_ds.each { |h| csv << h }
+end
+# File.open(filepath, 'wt') { |f| f.puts '"DOI","storage in bytes"'; error_ds.each { |doi,size| f.puts "\"#{doi}\",#{size}" } }
 
-puts "Storage errors report '#{File.basename(filepath)}' written. #{errors_ds.size} errors."
+puts "Storage errors report '#{File.basename(filepath)}' written. #{error_ds.size} errors."
