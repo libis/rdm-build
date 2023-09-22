359f959c53e6714cba42d7b4c8d616c831a0cd87
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2023-09-22 16:27:45 +0200

    fixed the wrong image tag in the script

--- images/dataverse/build_dv.sh
+++ images/dataverse/build_dv.sh
@@ -6,6 +6,6 @@ git clone --branch ${DATAVERSE_VERSION} --depth 1 https://github.com/IQSS/datave
 cd dataverse/modules/container-base
 mvn -Pct clean package -Dbase.image=${DATAVERSE_BASE_IMAGE_TAG} -Dbase.image.uid=${USER_ID} -Dbase.image.gid=${GROUP_ID}
 cd ../../../dataverse
-mvn -Pct clean package -Dbase.image=${DATAVERSE_BASE_IMAGE_TAG} -Dapp.image=${DATAVERSE_CONFIG_IMAGE_TAG} -Dconf.image=${DATAVERSE_CONFIG_IMAGE_TAG} -Dbase.image.uid=${USER_ID} -Dbase.image.gid=${GROUP_ID}
+mvn -Pct clean package -Dbase.image=${DATAVERSE_BASE_IMAGE_TAG} -Dapp.image=${DATAVERSE_STOCK_IMAGE_TAG} -Dconf.image=${DATAVERSE_CONFIG_IMAGE_TAG} -Dbase.image.uid=${USER_ID} -Dbase.image.gid=${GROUP_ID}
 cd ../..
 rm -rf temp
