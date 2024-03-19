ee235c25cc4457b83885746b8e9a22930f14eb39
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2024-03-19 12:50:50 +0100

    migration to asdf

--- images/dataverse/build_dv.sh
+++ images/dataverse/build_dv.sh
@@ -3,13 +3,6 @@
 mkdir temp
 cd temp
 git clone --branch ${DATAVERSE_VERSION} --depth 1 https://github.com/IQSS/dataverse.git
-wget https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz
-wget https://dlcdn.apache.org/maven/maven-3/3.9.5/binaries/apache-maven-3.9.5-bin.zip
-tar -xzf openjdk-17.0.2_linux-x64_bin.tar.gz
-unzip apache-maven-3.9.5-bin.zip
-export JAVA_HOME=$PWD/jdk-17.0.2
-export M2_HOME=$PWD/apache-maven-3.9.5
-export PATH="$JAVA_HOME/bin:$M2_HOME/bin:$PATH"
 cd dataverse/modules/container-base
 mvn -Pct clean package -Dbase.image=${DATAVERSE_BASE_IMAGE_TAG} -Dbase.image.uid=${USER_ID} -Dbase.image.gid=${GROUP_ID}
 cd ../../../dataverse
