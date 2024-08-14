6a9872da330cf60b179fd0c71a948e122eefb4f7
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2024-08-14 11:28:36 +0200

    build workaround for 6.3

--- images/dataverse/build_dv.sh
+++ images/dataverse/build_dv.sh
@@ -4,6 +4,9 @@ mkdir temp
 cd temp
 git clone --branch ${DATAVERSE_VERSION} --depth 1 https://github.com/IQSS/dataverse.git
 cd dataverse/modules/container-base
+cd src/main/docker
+wget -O Dockerfile https://raw.githubusercontent.com/IQSS/dataverse/develop/modules/container-base/src/main/docker/Dockerfile
+cd ../../..
 mvn -Pct clean package -Dbase.image=${DATAVERSE_BASE_IMAGE_TAG} -Dbase.image.uid=${USER_ID} -Dbase.image.gid=${GROUP_ID}
 cd ../../../dataverse
 mvn -Pct clean package -Dbase.image=${DATAVERSE_BASE_IMAGE_TAG} -Dapp.image=${DATAVERSE_STOCK_IMAGE_TAG} -Dconf.image=${DATAVERSE_CONFIG_IMAGE_TAG} -Dbase.image.uid=${USER_ID} -Dbase.image.gid=${GROUP_ID}
